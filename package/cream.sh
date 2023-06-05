367abb2579fb4d7af34486018a20277760877ce0
D. Neykov <metheorita@gmail.com>
2023-06-05 22:28:09 +0200

    Update script to automatically find libsteam_api.so

--- package/cream.sh
+++ package/cream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp "$PWD/lib32Creamlinux.so" /tmp/lib32Creamlinux.so
 cp "$PWD/lib64Creamlinux.so" /tmp/lib64Creamlinux.so
-cp "$PWD/libsteam_api.so" /tmp/libsteam_api.so
+cp "$PWD/$(find . -name "libsteam_api.so" -printf "%P\n" | head -n 1)" /tmp/libsteam_api.so
 LD_PRELOAD="$LD_PRELOAD /tmp/lib64Creamlinux.so /tmp/lib32Creamlinux.so /tmp/libsteam_api.so" "$@"
 EXITCODE=$?
 rm /tmp/lib32Creamlinux.so
